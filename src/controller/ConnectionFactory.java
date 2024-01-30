28555b884f66a9d59e0b4b44549d3685b9286f6d
hilucasalves <17169400+hilucasalves@users.noreply.github.com>
2024-01-30 07:50:30 -0300

    Acesso a arquivo de configuracao DB

--- src/controller/ConnectionFactory.java
+++ src/controller/ConnectionFactory.java
@@ -1,9 +1,12 @@
 package controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import static java.sql.DriverManager.getConnection;
 import static java.sql.DriverManager.registerDriver;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Logger;
 import static java.util.logging.Logger.getLogger;
 import static javax.swing.JOptionPane.showMessageDialog;
@@ -12,43 +15,42 @@ import static javax.swing.JOptionPane.showMessageDialog;
  *
  * @author INFOLUCK
  */
-public class ConnectionFactory{
-    
+public class ConnectionFactory {
+
     private static final Logger LOG = getLogger(ConnectionFactory.class.getName());
-    
-    /**
-     *
-     */
-    public Connection Connection;
-   
-    /**
-     *
-     */
-    public void openConnection(){
-    String server = "localhost"; //servidor do BD
-    String database ="papelaria2"; //nome do seu banco de dados
-    String url = "jdbc:mysql://" + server + "/" + database;//caminho
-    String user = "root"; //nome de um usuário de seu BD
-    String password = "InfoLuck79"; //sua senha de acesso
-    try{
-            registerDriver(new com.mysql.jdbc.Driver());
-    Connection = getConnection(url, user, password);
-    //JOptionPane.showMessageDialog(null,"Conexão com Banco de Dados realizada com sucesso.");
-    }
-    catch(SQLException SQLException){
-            showMessageDialog(null,"Erro: "+ SQLException.getMessage()); 
-    }}
 
     /**
      *
      */
-    public void closeConnection(){
-    try {
-    Connection.close();
-    //JOptionPane.showMessageDialog(null,"Banco de Dados desconectado.");
+    public Connection Connection;
+
+    /**
+     *
+     */
+    public void openConnection() {
+
+        try {
+            Properties props = new Properties();
+            FileInputStream input = new FileInputStream("database.properties");
+            props.load(input);
+
+            String url = "jdbc:mysql://" + props.getProperty("DB_SERVER") + ":" + props.getProperty("DB_PORT") + "/" + props.getProperty("DB_NAME");//caminho
+            registerDriver(new com.mysql.cj.jdbc.Driver());
+            Connection = getConnection(url, props.getProperty("DB_USER"), props.getProperty("DB_PASS"));
+        } catch (SQLException | IOException ex) {
+            showMessageDialog(null, "Erro: " + ex.getMessage());
+        }
     }
-    catch (SQLException SQLException){
-            showMessageDialog(null,"Erro: "+ SQLException.getMessage()); 
-    }}  
-        
+
+    /**
+     *
+     */
+    public void closeConnection() {
+        try {
+            Connection.close();
+        } catch (SQLException SQLException) {
+            showMessageDialog(null, "Erro: " + SQLException.getMessage());
+        }
+    }
+
 }
